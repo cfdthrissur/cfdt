f0768bcb5de532b9459d6291bff54d3dbdab213c
ccf gect <cfdthrissur@gmail.com>
2018-06-20 18:04:23 +0530

    20062018-6PM

--- lsgd/admin.py
+++ lsgd/admin.py
@@ -2,11 +2,15 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from .models import District
+from lsgd.models import District, Lsgd
 
 # Register your models here.
 class DistrictAdmin(admin.ModelAdmin):
-    list_display=('district_code','district_name')
+    list_display=('district_code', 'district_name')
+
+class LsgdAdmin(admin.ModelAdmin):
+    list_display=('lsgd_code', 'lsgd_name', 'lsgd_type')
 
 
-admin.site.register(District,DistrictAdmin)
+admin.site.register(District, DistrictAdmin)
+admin.site.register(Lsgd, LsgdAdmin)
--- lsgd/models.py
+++ lsgd/models.py
@@ -4,6 +4,12 @@ from django.db import models
 from django.contrib import admin
 
 # Create your models here.
-class district(models.Model):
-    district_code=models.CharField(max_length=10,unique=True)
-    district_name=models.CharField(max_length=50,unique=True)
+
+class District(models.Model):
+    district_code = models.CharField(max_length = 10, unique = True, primary_key=True)
+    district_name = models.CharField(max_length = 50, unique = True)
+   
+class Lsgd(models.Model):
+    lsgd_code = models.CharField(max_length = 15, unique = True, primary_key = True)
+    lsgd_name = models.CharField(max_length = 100, unique = True)
+    lsgd_type = models.CharField(max_length = 15)
